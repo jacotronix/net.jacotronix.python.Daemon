ebab826fcd1a1c8996fd50e2d7c8bcff114dbb17
Jamie <jamie.d.jackson@gmail.com>
2012-12-27 19:00:34 +0000

    First commit

---
+++ Daemon/README.txt
@@ -0,0 +1,6 @@
+Daemon Wrapper
+
+Code from this from Sander Marechal's “A simple unix/linux daemon in Python"
+http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
+0.0.1	27 Dec 12	Wrapper for Unix style Daemon processes
---
+++ Daemon/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+setup(name='daemon',
+      version='0.0.1',
+      description='Daemon Wrapper',
+      maintainer='Jamie Jackson',
+      maintainer_email='jamie@jacobean.net',
+      url='http://blog.jacobean.net',
+      py_modules=['daemon'],
+      )
